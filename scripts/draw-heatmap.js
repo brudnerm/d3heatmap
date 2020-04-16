0908d53319e9e55783dd41cecf01f6ae2eded21f
brudnerm <brudner.m@husky.neu.edu>
2020-04-16 16:27:53 -0400

    get unique array values for x and y axis, more tidying

--- scripts/draw-heatmap.js
+++ scripts/draw-heatmap.js
@@ -1,115 +1,141 @@
 //https://www.d3-graph-gallery.com/graph/heatmap_basic.html
 
-    // set the dimensions and margins of the graph
-    var figureSize = 350;
-    var margin = {
-            top: 65,
-            right: 65,
-            bottom: 65,
-            left: 65
-        },
-        width = figureSize - margin.left - margin.right,
-        height = figureSize - margin.top - margin.bottom;
+////// SETUP SVG
+    // Set the dimensions and margins of the graph
+var figureSize = 800;
+var margin = {
+        top: 65,
+        right: 65,
+        bottom: 65,
+        left: 65
+    },
+    width = figureSize - margin.left - margin.right,
+    height = figureSize * .5 - margin.top - margin.bottom;
 
-    // append the svg object to the body of the page
-    var svg = d3.select("#heatmap")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
+    // Append the svg object to the body of the page
+var svg = d3.select("#heatmap")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
 
-    // Labels of row and columns
-    var axisLabelsX = ["gene_A", "gene_B", "gene_C", "gene_D", "gene_E", "gene_F", "gene_G", "gene_H", "gene_I", "gene_J"]
-    var axisLablesY = ["cell_type_10", "cell_type_9", "cell_type_8", "cell_type_7", "cell_type_6", "cell_type_5", "cell_type_4", "cell_type_3", "cell_type_2", "cell_type_1"]
+////// DEFINE FUNCTION FOR DRAWING HEATMAP
+function drawHeatmap(data) {
+    console.log(data[0])
 
+    // SET UP DIV FOR TOOLTIP
+    var div = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+    
+    // Set whitespace between heatmap squares
+    var whitespace = 0.075;
+
+////// SET UP X AXIS
+    // Get all unique values for X axis
+    let axisLabelsX = [...new Set(data.map(item => item.genes))];
     // Build X scales and axis:
     var x = d3.scaleBand()
         .range([0, width])
         .domain(axisLabelsX)
-        .padding(0.05);
+        // X padding
+        .padding(whitespace);
 
-    svg.append("g")
-        // Call X axis
-        .call(d3.axisTop(x))
-        //Remove tickmarks from X axis
+    // Refer to X axis
+    svg.append("g").call(d3.axisTop(x))
+    // Remove tickmarks from X axis
         .attr('stroke-width', 0)
-        // Rotate X-axis labels
+        // Set up X axis labels
         .selectAll("text")
-        .style("text-anchor", "start")
-        .attr("transform", "rotate(-45)")
-        .attr("dy", "0")
-        .attr("dx", ".5em");
-
-    // Define the div for the tooltip
-    var div = d3.select("body").append("div")
-        .attr("class", "tooltip")
-        .style("opacity", 0);
-
-    // Build X scales and axis:
+            // Rotate X-axis labels
+            .style("text-anchor", "start")
+            .attr("transform", "rotate(-45)")
+            .attr("dy", "0")
+            .attr("dx", ".5em")
+            // X axis font size
+            .attr("font-size", "8px");
+    
+////// SET UP Y AXIS
+    // Get all unique values for Y axis
+    let axisLabelsY = [...new Set(data.map(item => item.cells))]
+        // sort Y axis alphabetically
+        .sort(d3.descending);
+    
+    // Build Y scales and axis:
     var y = d3.scaleBand()
         .range([height, 0])
-        .domain(axisLablesY)
-        .padding(0.05);
+        .domain(axisLabelsY)
+        // Y padding
+        .padding(whitespace);
 
-    // Remove tickmarks from Y axis
-    svg.append("g")
-        .call(d3.axisLeft(y))
-        .attr('stroke-width', 0);
+    // Refer to Y axis
+    svg.append("g").call(d3.axisLeft(y))
+            // Remove tickmarks from Y axis
+            .attr('stroke-width', 0);
 
-    // Build color scale from "white" to "intellia orange"
+////// SET UP COLOR SCALE FOR HEATMAP            
     var myColor = d3.scaleLinear()
+        // Define color scale from "white" to "intellia orange"
         .range(["white", "#d34927"])
         // Domain fixed, change to max/min of min/max of values?
         .domain([1, 1000])
 
+////// SET UP THE HEATMAP
+    //Read the data and cross link the variables
+    svg.selectAll()
+    .data(data, function(d) {
+        return d.genes + ':' + d.cells;
+    })
 
-    // get CSV data
-    d3.csv("data/heatmap_data.csv").then(function(data) {
+    // Make heatmap squares for each row in CSV
+    .enter()
+    .append("rect")
 
-        //Read the data
-        svg.selectAll()
-            .data(data, function(d) {
-                return d.genes + ':' + d.cells;
-            })
+    // Place heatmap squares according to data
+    .attr("x", function(d) {
+        return x(d.genes)
+    })
+    .attr("y", function(d) {
+        return y(d.cells)
+    })
 
-            // Make Recangles for each row in CSV
-            .enter()
-            .append("rect")
+    // Size heatmap squares
+    .attr("width", x.bandwidth())
+    .attr("height", y.bandwidth())
 
-            // Place Rectangles according to data
-            .attr("x", function(d) {
-                return x(d.genes)
-            })
-            .attr("y", function(d) {
-                return y(d.cells)
-            })
+    // Fill heatmap colors by value
+    .style("fill", function(d) {
+        return myColor(d.value)
+    })
 
-            // Size Rectangles
-            .attr("width", x.bandwidth())
-            .attr("height", y.bandwidth())
+    // Start tooltip on mouesover
+    .on("mouseover", function(d) {
+        // Set opacity and transition
+        div.transition()
+            .duration(200)
+            .style("opacity", .9);
+        // Define tooltip labels and data
+        div.html(d.cells + "<br/>" + d.genes + "<br/>" + "value: " + d.value)
+            .style("left", (d3.event.pageX) + "px")
+            .style("top", (d3.event.pageY - 50) + "px");
+    })
 
-            // Fill heatmap colors by data=value
-            .style("fill", function(d) {
-                return myColor(d.value)
-            })
-            // Heatmap Circles
-            .attr("rx", 20)
-            .attr("ry", 20)
-
-            .on("mouseover", function(d) {
-                div.transition()
-                    .duration(200)
-                    .style("opacity", .9);
-                div.html(d.cells + "<br/>" + d.genes + "<br/>" + "value: " + d.value)
-                    .style("left", (d3.event.pageX) + "px")
-                    .style("top", (d3.event.pageY - 50) + "px");
-            })
-            .on("mouseout", function(d) {
-                div.transition()
-                    .duration(500)
-                    .style("opacity", 0);
-            });
-
+    // Kill tooltip on mouseout
+    .on("mouseout", function(d) {
+        // Set opacity and transitio
+        div.transition()
+            .duration(500)
+            .style("opacity", 0);
     });
+
+};
+
+////// GET THE CSV DATA
+d3.csv("data/heatmap_data.csv").then(function(data) {
+    
+    // Draw heatmap from data
+    drawHeatmap(data)
+
+});
